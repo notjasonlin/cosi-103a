17a02f3e309f9ab017bc9da4f9d182990eb59f37
Altakie <90630839+Altakie@users.noreply.github.com>
2024-01-22 01:16:27 +0000

    added time travel

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -8,9 +8,19 @@ function Square({value, onSquareClick}) {
     </button>);
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function calculateWinner(squares) {
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+function Board({xIsNext, squares, onPlay}) {
 
   const winner = calculateWinner(squares);
   let status;
@@ -31,8 +41,7 @@ export default function Board() {
       nextSquares[i] = 'O';
     }
     
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
+    onPlay(nextSquares);
   }
 
   return (<>
@@ -58,14 +67,46 @@ export default function Board() {
   );
 }
 
-function calculateWinner(squares) {
-  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+ 
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0,currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1)
+    setXIsNext(!xIsNext);
   }
 
-  return null;
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+ }
+
+ const moves = history.map((squares, move) => {
+  let description;
+  if (move > 0) {
+    description = 'Go to move #' + move;
+  } else {
+    description = 'Go to game start';
+  }
+ return (
+  <li key={move}>
+    <button onClick={() => jumpTo(move)}>{description}</button>
+  </li>
+ )
+});
+
+  return (<>
+  <div className='game'> 
+    <div className="game-board">
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}> </Board>
+    </div>
+    <div className='game-info'>
+      <ol> {moves}</ol>
+    </div>
+  </div>
+  </>);
 }
