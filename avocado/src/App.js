1ea5b2904f067a620921bd35ca72ac68bcd029d8
elan-romo <114878476+elan-romo@users.noreply.github.com>
2024-01-29 21:28:56 +0000

    Elan and John fixed CSS Appearance of Site, added a footer, and uploaded a logo and favicon

--- avocado/src/App.js
+++ avocado/src/App.js
@@ -6,11 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Avocado
+        <img src="/images/avocado-transparent.png" alt="Logo" className="App-logo" />
+        <div className="Header-content">
+          <div>Avocado Inc.</div>
+          <div className="Subtitle">Delicious Recipes</div>
+        </div>
       </header>
-      <DropDown />
+
+      <div className="App-body">
+        <DropDown />
+      </div>
+
+      <footer className="footer">
+        <h2 id="copyright">© 2024 Avocado Inc.</h2>
+        <a href="https://facebook.com" class="fa fa-facebook" ></a>
+        <a href="https://twitter.com" class="fa fa-twitter" ></a>
+        <a href="https://youtube.com" class="fa fa-youtube"></a>
+        <a href="https://instagram.com" class="fa fa-instagram"></a>
+        <a href="https://linkedin.com" class="fa fa-linkedin"></a>
+      </footer>
     </div>
   );
 }
+
 
 export default App;
