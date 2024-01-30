f92ebda05cadf47478e2536330f3c26070f6eeff
Altakie <90630839+Altakie@users.noreply.github.com>
2024-01-30 01:42:21 +0000

    wrote a working jest test!

--- avocado/src/App.test.js
+++ avocado/src/App.test.js
@@ -1,9 +1,10 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import App from "./App";
+import { computeStyles } from "@popperjs/core";
 
-test("Renders Avocado Inc.", () => {
+test("renders without crashing", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/Avocado Inc./i);
-	expect(linkElement).toBeInTheDocument();
+	const linkElements = screen.queryAllByText("Avocado Inc.");
+	expect(linkElements.length).toBeGreaterThan(0);
 });
