0595c1ac1a62ff779cbeb746ac00a5f7aa784ca9
Jason Lin <113144409+notjasonlin@users.noreply.github.com>
2024-01-30 00:29:05 +0000

    bable

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    '@babel/plugin-syntax-jsx',
+    'module:@babel/plugin-syntax-jsx',
     '@babel/preset-env',
     ['@babel/preset-react', {runtime: 'automatic'}],
   ],
