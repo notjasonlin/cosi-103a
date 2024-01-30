36d447f84cf561e75ed4516cc7121a778d87df3e
Altakie <90630839+Altakie@users.noreply.github.com>
2024-01-30 02:39:55 +0000

    brain hurt

--- avocado/src/App.test.js
+++ avocado/src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App";
-import { computeStyles } from "@popperjs/core";
 
 test("renders without crashing", () => {
 	render(<App />);
--- avocado/src/dropdown.js
+++ avocado/src/dropdown.js
@@ -1,46 +1,48 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Recipes from './recipes';
-import '/workspaces/cosi-103a/avocado/src/dropDown.css';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import Recipes from "./recipes";
 
 const DropDown = () => {
-    const [selectedRecipe, setSelectedRecipe] = useState('');
+	const [selectedRecipe, setSelectedRecipe] = useState("");
 
-    const handleSelectRecipe = (recipeName) => {
-        setSelectedRecipe(recipeName);
-    };
+	const handleSelectRecipe = (recipeName) => {
+		setSelectedRecipe(recipeName);
+	};
 
-    const resetView = () => {
-        setSelectedRecipe(''); // Resetting the selected recipe
-    };
+	const resetView = () => {
+		setSelectedRecipe(""); // Resetting the selected recipe
+	};
 
-    const renderRecipe = () => {
-        const RecipeComponent = Recipes[selectedRecipe];
-        return RecipeComponent ? <RecipeComponent /> : null;
-    };
+	const renderRecipe = () => {
+		const RecipeComponent = Recipes[selectedRecipe];
+		return RecipeComponent ? <RecipeComponent /> : null;
+	};
 
-    return (
-        <Dropdown as={ButtonGroup}>
-            {/* Reset view on button click */}
-            <Button variant="success" onClick={resetView}>Avocado Recipes</Button>
+	return (
+		<Dropdown as={ButtonGroup}>
+			{/* Reset view on button click */}
+			<Button variant="success" onClick={resetView}>
+				Avocado Recipes
+			</Button>
 
-            <Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
+			<Dropdown.Toggle split variant="success" id="dropdown-split-basic" />
 
-            <Dropdown.Menu>
-                {Object.keys(Recipes).map((recipeName, index) => (
-                    <Dropdown.Item key={index} onClick={() => handleSelectRecipe(recipeName)}>
-                        {recipeName}
-                    </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
+			<Dropdown.Menu>
+				{Object.keys(Recipes).map((recipeName, index) => (
+					<Dropdown.Item
+						key={index}
+						onClick={() => handleSelectRecipe(recipeName)}
+					>
+						{recipeName}
+					</Dropdown.Item>
+				))}
+			</Dropdown.Menu>
 
-            <div id="display">
-                {renderRecipe()}
-            </div>
-        </Dropdown>
-    );
+			<div id="display">{renderRecipe()}</div>
+		</Dropdown>
+	);
 };
 
 export default DropDown;
