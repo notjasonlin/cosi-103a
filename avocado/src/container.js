c8673321a1e4a40c2446bd3fd396160079ac8b3b
elan-romo <114878476+elan-romo@users.noreply.github.com>
2024-02-05 00:27:10 +0000

    Elan and Artem made all cards appear on site

--- avocado/src/container.js
+++ avocado/src/container.js
@@ -5,29 +5,15 @@ import recipesData from "./data/recipeData";
 
 import RecipeCard from "./card";
 
-function mapCardArrayToComponent(cardArray) {
-	return cardArray.map((card, index) => (
-		<Col key={index}>
-			<RecipeCard recipeId={card.id} />
-		</Col>
-	));
-}
-
-function makeRecipeCards() {
-	recipesData.forEach((recipe) => {
-		return <RecipeCard recipeId={recipe.id} />;
-	});
-}
-
 function RecipeContainer() {
 	return (
 		<Container>
-			{recipesData.map((recipe) => {
-				<RecipeCard recipeId={recipe.id} />;
-			})}
-			<RecipeCard recipeId={1} />
+			{recipesData.map((recipe) => (
+				<RecipeCard key={recipe.id} recipeId={recipe.id} />
+			))}
 		</Container>
 	);
 }
+
 
 export default RecipeContainer;
