d7cf15f7371403b8268bd403755e9f58d6b3ba24
Altakie <90630839+Altakie@users.noreply.github.com>
2024-02-06 19:37:40 +0000

    tried to fix tests

--- avocado/src/tests/card.test.js
+++ avocado/src/tests/card.test.js
@@ -1,12 +1,23 @@
-import { render, screen , waitFor} from "@testing-library/react";
+import { render, screen, waitFor, cleanup } from "@testing-library/react";
 import React from "react";
 import RecipeCard from "../components/card";
-import recipesData from "../data/recipeData";
+
+afterEach(() => {
+	cleanup();
+});
 
 test("renders properly", async () => {
-    render(<RecipeCard id={1} />);
-    await waitFor(() => {
-        const linkElements = screen.getAllByText("Avocado Toast");
-        expect(linkElements.length).toBeGreaterThan(0);
-    });
+	const testRecipe = {
+		id: 1,
+		name: "Avocado Toast",
+		description:
+			"Toasted bread topped with mashed avocado, seasoned with salt, pepper, and a splash of citrus juice. Customize with toppings like tomatoes or poached eggs for a quick and tasty meal.",
+		image: "/images/avocado-toast.png",
+	};
+
+	render(<RecipeCard key={testRecipe} recipe={testRecipe} />);
+	await waitFor(() => {
+		const linkElements = screen.getAllByText("Avocado Toast");
+		expect(linkElements.length).toBeGreaterThan(0);
+	});
 });
