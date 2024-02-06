5a48ef69a192f4fc0032b24b3f84b593deffcd08
JohnV119 <156862458+JohnV119@users.noreply.github.com>
2024-02-06 18:50:35 +0000

    Fixed card tests, removed dropdown components

--- avocado/src/tests/card.test.js
+++ avocado/src/tests/card.test.js
@@ -1,10 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen , waitFor} from "@testing-library/react";
 import React from "react";
 import RecipeCard from "../components/card";
 import recipesData from "../data/recipeData";
 
-test("renders properly", () => {
-	render(<RecipeCard id={1} />);
-	const linkElements = screen.queryAllByText("Avocado Toast");
-	expect(linkElements.length).toBeGreaterThan(0);
+test("renders properly", async () => {
+    render(<RecipeCard id={1} />);
+    await waitFor(() => {
+        const linkElements = screen.getAllByText("Avocado Toast");
+        expect(linkElements.length).toBeGreaterThan(0);
+    });
 });
