3d97777771a68ef80cebbb60e42e7c44e55917cb
Jason Lin <113144409+notjasonlin@users.noreply.github.com>
2024-02-07 20:50:44 +0000

    added descriptionn

--- avocado/src/data/memberData.js
+++ avocado/src/data/memberData.js
@@ -3,28 +3,28 @@ const memberData = [
         id: 0,
         name: "Artem Lavrov",
         description: 
-            "",
+            "This is Artem, he is a rat!",
         image: "/images/ratAvocado.png",
     },
     {
         id: 1,
         name: "Elan Romo",
         description: 
-            "",
+            "This is Elan, he is an avocado!",
         image: "/images/ratAvocado.png",
     },
     {
         id: 2,
         name: "Jason Lin",
         description: 
-            "",
+            "This is Jason, he is a dog!",
         image: "/images/ratAvocado.png",
     },
     {
         id: 3,
         name: "John Vnek",
         description: 
-            "",
+            "This is John, he is a bird!",
         image: "/images/ratAvocado.png",
     }
 ]
