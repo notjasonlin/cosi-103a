e3b92973718b3ef2054ccb101c5ea56080e2c1ef
JohnV119 <156862458+JohnV119@users.noreply.github.com>
2024-02-07 20:35:37 +0000

    started adding team member card and data

---
+++ avocado/src/components/teamcard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+// Creates a recipe card containing all recipe data
+function TeamCard({ member }) {
+	if (!member) {
+		return <div>Member not found!</div>;
+	}
+
+	return (
+		<Card
+			text="light"
+			border="success"
+			style={{
+				backgroundColor: "#272727",
+				width: "30%",
+				borderRadius: "3rem",
+				margin: "1rem",
+				borderColor: "green",
+				borderWidth: "0.5rem",
+				boxShadow: "0 0 0.5rem 0.3rem green",
+			}}
+		>
+			<Card.Img
+				variant="top"
+				src={Member.image}
+				alt={Member.name}
+				style={{
+					width: "100%",
+					height: "20vw",
+					objectFit: "cover",
+					padding: "1rem",
+					borderRadius: "2rem",
+				}}
+			/>
+			<Card.Body>
+				<Card.Title>{member.name}</Card.Title>
+				<Card.Text>{member.description}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+}
+
+export default RecipeCard;
---
+++ avocado/src/data/memberData.js
@@ -0,0 +1,9 @@
+const memberData = [
+    {
+        id: 0,
+        name: "Artem Lavrov",
+        description: 
+            "",
+        
+    }
+]
