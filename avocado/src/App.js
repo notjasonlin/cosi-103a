d1fc59755e1fd8ff8c913738e2d17ac8378b6a81
Altakie <90630839+Altakie@users.noreply.github.com>
2024-02-08 02:13:00 +0000

    made it so that the avocado is a link to the home page

--- avocado/src/App.js
+++ avocado/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	useParams,
+	Link,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./cssfiles/App.css";
 import RecipeContainer from "./components/recipeContainer";
@@ -7,42 +13,47 @@ import RecipeList from "./components/recipes";
 import HomePage from "./components/home-page";
 import TeamContainer from "./components/teamContainer";
 
-
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src="/images/avocado-transparent.png" alt="Logo" className="App-logo" />
-          <div className="Header-content">
-            <div>Avocado Inc.</div>
-            <div className="Subtitle">Delicious Recipes</div>
-          </div>
-        </header>
+	return (
+		<Router>
+			<div className="App">
+				<header className="App-header">
+					<Link to="/" className="Logo-Container">
+						<img
+							src="/images/avocado-transparent.png"
+							alt="Logo"
+							className="App-logo"
+						/>
+					</Link>
+					<div className="Header-content">
+						<div>Avocado Inc.</div>
+						<div className="Subtitle">Delicious Recipes</div>
+					</div>
+				</header>
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/team" element={<TeamContainer />} />
-          <Route path="/recipe/:recipeId" element={<RecipeListPage />} />
-          <Route path="/recipes" element={<RecipeContainer />} />
-        </Routes>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/team" element={<TeamContainer />} />
+					<Route path="/recipe/:recipeId" element={<RecipeListPage />} />
+					<Route path="/recipes" element={<RecipeContainer />} />
+				</Routes>
 
-        <footer className="footer">
-          <h2 id="copyright">© 2024 Avocado Inc.</h2>
-          <a href="https://facebook.com" className="fa fa-facebook"></a>
-          <a href="https://twitter.com" className="fa fa-twitter"></a>
-          <a href="https://youtube.com" className="fa fa-youtube"></a>
-          <a href="https://instagram.com" className="fa fa-instagram"></a>
-          <a href="https://linkedin.com" className="fa fa-linkedin"></a>
-        </footer>
-      </div>
-    </Router>
-  );
+				<footer className="footer">
+					<h2 id="copyright">© 2024 Avocado Inc.</h2>
+					<a href="https://facebook.com" className="fa fa-facebook"></a>
+					<a href="https://twitter.com" className="fa fa-twitter"></a>
+					<a href="https://youtube.com" className="fa fa-youtube"></a>
+					<a href="https://instagram.com" className="fa fa-instagram"></a>
+					<a href="https://linkedin.com" className="fa fa-linkedin"></a>
+				</footer>
+			</div>
+		</Router>
+	);
 }
 
 function RecipeListPage() {
-  const { recipeId } = useParams(); // Extract recipeId from URL params
-  return <RecipeList recipeId={recipeId} />;
+	const { recipeId } = useParams(); // Extract recipeId from URL params
+	return <RecipeList recipeId={recipeId} />;
 }
 
 export default App;
