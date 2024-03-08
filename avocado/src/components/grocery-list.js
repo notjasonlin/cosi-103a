d9c6d82169dd16532a19dcd6cca6c16fcdd71a6c
elan-romo <114878476+elan-romo@users.noreply.github.com>
2024-03-08 23:37:58 +0000

    Added CSS for the Grocery list

--- avocado/src/components/grocery-list.js
+++ avocado/src/components/grocery-list.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { useGroceryContext } from '../data/grocery-context';
+import '../cssfiles/groceries/groceryList.css';
 
 const GroceryList = () => {
-  const { items, removeGrocery, removeAllGrocery} = useGroceryContext();
+  const { items, removeGrocery, removeAllGrocery } = useGroceryContext();
 
   return (
-    <div>
-      <h2>Grocery List</h2>
-      <button onClick={() => removeAllGrocery()}>Clear</button>
+    <div className="grocery-list-container">
+      <h2 className="grocery-list-title">Grocery List</h2>
+      <button className="grocery-list-button" onClick={() => removeAllGrocery()}>Clear</button>
       <ul>
         {items.map((item) => (
           <li key={item.id}>
             {item.item}{' '}
-            <button onClick={() => removeGrocery(item.id)}>X</button>
+            <button className="item-remove-button" onClick={() => removeGrocery(item.id)}>X</button>
           </li>
         ))}
       </ul>
