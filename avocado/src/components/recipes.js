b2bbc6757c6b2e7528d4f2f667dee7faa057e560
Altakie <90630839+Altakie@users.noreply.github.com>
2024-03-10 20:26:13 +0000

    made data folder

--- avocado/src/components/recipes.js
+++ avocado/src/components/recipes.js
@@ -1,62 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import recipesData from "../data/recipeData.js";
-import { useGroceryContext } from '../data/grocery-context';
+import { useGroceryContext } from "../data/grocery-context";
 import "../cssfiles/dropDown.css";
 import "../cssfiles/recipes.css";
 
 const RecipeList = ({ recipeId }) => {
-  const { addGrocery } = useGroceryContext();
+	const { addGrocery } = useGroceryContext();
 
-  // Convert recipeId to an integer
-  const id = parseInt(recipeId);
+	// Convert recipeId to an integer
+	const id = parseInt(recipeId);
 
-  // Find the recipe with the specified id
-  const selectedRecipe = recipesData.find((recipe) => recipe.id === id);
+	// Find the recipe with the specified id
+	const selectedRecipe = recipesData.find((recipe) => recipe.id === id);
 
-  // Check if a recipe with the specified id exists
-  if (!selectedRecipe) {
-    return <div></div>;
-  }
+	// Check if a recipe with the specified id exists
+	if (!selectedRecipe) {
+		return <div></div>;
+	}
 
-  const handleAddToGrocery = (ingredient) => {
-    addGrocery(ingredient);
-  };
+	const handleAddToGrocery = (ingredient) => {
+		addGrocery(ingredient);
+	};
 
-  return (
-    <div className="recipe-list">
-      <div key={selectedRecipe.id} className="recipe" data-testid="recipe-card">
-        <h1>{selectedRecipe.name}</h1>
-        <img src={selectedRecipe.image} alt={selectedRecipe.name} />
-        <div className="description">
-          <p>{selectedRecipe.description}</p>
-        </div>
-        <div className="ingredients">
-          <h2>Ingredients</h2>
-          <ul>
-            {selectedRecipe.ingredients.map((ingredient, index) => (
-              <li key={index}>
-                {ingredient}{" "}
-                <button onClick={() => handleAddToGrocery(ingredient)}>
-                  +
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="instructions">
-          <h2>Instructions</h2>
-          <ol>
-            {selectedRecipe.instructions.map((instruction, index) => (
-              <li key={index}>{instruction}</li>
-            ))}
-          </ol>
-        </div>
-        {/* Back button */}
-        <Link to="/">Go Back</Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="recipe-list">
+			<div key={selectedRecipe.id} className="recipe" data-testid="recipe-card">
+				<h1>{selectedRecipe.name}</h1>
+				<img src={selectedRecipe.image} alt={selectedRecipe.name} />
+				<div className="description">
+					<p>{selectedRecipe.description}</p>
+				</div>
+				<div className="ingredients">
+					<h2>Ingredients</h2>
+					<ul>
+						{selectedRecipe.ingredients.map((ingredient, index) => (
+							<li key={index}>
+								{ingredient}{" "}
+								<button onClick={() => handleAddToGrocery(ingredient)}>
+									+
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="instructions">
+					<h2>Instructions</h2>
+					<ol>
+						{selectedRecipe.instructions.map((instruction, index) => (
+							<li key={index}>{instruction}</li>
+						))}
+					</ol>
+				</div>
+				{/* Back button */}
+				<Link to="/">Go Back</Link>
+			</div>
+		</div>
+	);
 };
 
 export default RecipeList;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/api", (req, res) => {});
 
-})
+app.post("/api", (req, res) => {});
 
-app.listen(5000, () => { console.log("Server started on port 5000") })
+app.listen(5000, () => {
+	console.log("Server started on port 5000");
+});
