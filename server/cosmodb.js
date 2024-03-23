28dd882e9c8570d7aa90d065967b2bd3ef50cc68
Jason Lin <jasonlin@brandeis.edu>
2024-03-23 12:13:58 -0400

    added keys to env

--- server/cosmodb.js
+++ server/cosmodb.js
@@ -1,21 +1,44 @@
-const credential = new DefaultAzureCredential();
+const { CosmosClient } = require('@azure/cosmos');
+const { ClientSecretCredential } = require('@azure/identity');
+import { setLogLevel } from "@azure/logger";
 
-const client = new CosmosClient({
-    endpoint,
-    aadCredentials: credential
-});
+setLogLevel("info");
+require('dotenv').config(); // Make sure this comes before using process.env
 
-const database = client.database('cosmicworks');
-const container = database.container('products');
+const endpoint = process.env.COSMOS_ENDPOINT || 'https://cosi103cosmosdbdatabase.documents.azure.com:443/';
+const tenantId = process.env.AZURE_TENANT_ID;
+const clientId = process.env.AZURE_CLIENT_ID;
+const clientSecret = process.env.AZURE_CLIENT_SECRET;
 
-var item = {
-    'id': '70b63682-b93a-4c77-aad2-65501347265f',
-    'category': 'gear-surf-surfboards',
-    'name': 'Yamba Surfboard',
-    'quantity': 12,
-    'price': 850.00,
-    'clearance': false
-};
+const credential = new DefaultAzureCredential({
+    loggingOptions: { allowLoggingAccountIdentifiers: true },
+  });
 
-var response = await container.items.upsert(item);
-console.log(item);
+async function upsertItem() {
+    const client = new CosmosClient({
+        endpoint,
+        aadCredentials: credential
+    });
+
+    const database = client.database('cosi103cosmosdbdatabase');
+    const container = database.container('recipe1');
+
+    var item = {
+        'id': '70b63682-b93a-4c77-aad2-65501347265f',
+        'category': 'gear-surf-surfboards',
+        'name': 'Yamba Surfboard',
+        'quantity': 12,
+        'price': 850.00,
+        'clearance': false
+    };
+
+    try {
+        var response = await container.items.upsert(item);
+        console.log('Upserted item:', response.resource);
+    } catch (error) {
+        console.error('Error upserting item:', error);
+    }
+}
+
+// Call the async function
+upsertItem();
