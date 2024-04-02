ecb0c3f52c24515d6560ad58a35ca333dfcf8c44
Altakie <90630839+Altakie@users.noreply.github.com>
2024-04-02 01:34:06 +0000

    fixed recipes

--- avocado/src/components/recipes.js
+++ avocado/src/components/recipes.js
@@ -1,78 +1,92 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
-import { useGroceryContext } from '../data/grocery-context';
+import { Button } from "react-bootstrap";
+import { useGroceryContext } from "../data/grocery-context";
 import "../cssfiles/dropDown.css";
 import "../cssfiles/recipes.css";
 
 const RecipeList = ({ recipeId, recipesData }) => {
+	// console.log("recipesData: ", recipesData);
 
-  const { addGrocery } = useGroceryContext();
+	const { addGrocery } = useGroceryContext();
 
-  // Convert recipeId to an integer
-  const id = parseInt(recipeId);
+	// Convert recipeId to an integer
+	// const id = recipeId;
 
-  // Find the recipe with the specified id
-  const selectedRecipe = recipesData.find((recipe) => recipe.id === id);
+	// Find the recipe with the specified id
+	const selectedRecipe = recipesData.find((recipe) => recipe.id === recipeId);
 
-  // Check if a recipe with the specified id exists
-  if (!selectedRecipe) {
-    return <div></div>;
-  }
+	// Check if a recipe with the specified id exists
+	if (!selectedRecipe) {
+		console.log("tragedy occured");
+		return <div></div>;
+	}
 
-  const handleAddToGrocery = (ingredient) => {
-    addGrocery(ingredient);
-  };
+	const handleAddToGrocery = (ingredient) => {
+		addGrocery(ingredient);
+	};
 
-  return (
-    <div className="recipe-list">
-      <div key={selectedRecipe.id} className="recipe" data-testid="recipe-card">
-        <h1>{selectedRecipe.name}</h1>
-        <Link to={`/recipe/${recipeId}/cookingMode`} style={{ textDecoration: "none" }}>
-          <Button variant="success" style={{ borderRadius: "2rem" }}>
-            Cooking Mode
-          </Button>
-        </Link>
-        <img src={selectedRecipe.image} alt={selectedRecipe.name} />
-        <div className="description">
-          <p>{selectedRecipe.description}</p>
-        </div>
-        <div className="ingredients">
-          <h2>Ingredients</h2>
-          <ul>
-            {selectedRecipe.ingredients.map((ingredientObj, index) => (
-              <li key={index}>
-                {Object.keys(ingredientObj).map(ingredientKey => (
-                  <span key={ingredientKey}>
-                    <a href={`https://fdc.nal.usda.gov/fdc-app.html#/food-details/${ingredientObj[ingredientKey]}/nutrients`} className="ingredient-link" onClick={(e) => {
-                      // Your existing onClick handler
-                    }}>
-                      {ingredientKey}
-                    </a>{" "}
-                    <br />
-                    <button className="addButton" onClick={() => handleAddToGrocery(ingredientKey)}>
-                      +
-                    </button>
-                    <br />
-                  </span>
-                ))}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="instructions">
-          <h2>Instructions</h2>
-          <ol>
-            {selectedRecipe.instructions.map((instruction, index) => (
-              <li key={index}>{instruction}</li>
-            ))}
-          </ol>
-        </div>
-        {/* Back button */}
-        <Link to="/" className="goBackButton">Go Back</Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="recipe-list">
+			<div key={selectedRecipe.id} className="recipe" data-testid="recipe-card">
+				<h1>{selectedRecipe.name}</h1>
+				<Link
+					to={`/recipe/${recipeId}/cookingMode`}
+					style={{ textDecoration: "none" }}
+				>
+					<Button variant="success" style={{ borderRadius: "2rem" }}>
+						Cooking Mode
+					</Button>
+				</Link>
+				<img src={selectedRecipe.image} alt={selectedRecipe.name} />
+				<div className="description">
+					<p>{selectedRecipe.description}</p>
+				</div>
+				<div className="ingredients">
+					<h2>Ingredients</h2>
+					<ul>
+						{selectedRecipe.ingredients.map((ingredientObj, index) => (
+							<li key={index}>
+								{Object.keys(ingredientObj).map((ingredientKey) => (
+									<span key={ingredientKey}>
+										<a
+											href={`https://fdc.nal.usda.gov/fdc-app.html#/food-details/${ingredientObj[ingredientKey]}/nutrients`}
+											className="ingredient-link"
+											onClick={(e) => {
+												// Your existing onClick handler
+											}}
+										>
+											{ingredientKey}
+										</a>{" "}
+										<br />
+										<button
+											className="addButton"
+											onClick={() => handleAddToGrocery(ingredientKey)}
+										>
+											+
+										</button>
+										<br />
+									</span>
+								))}
+							</li>
+						))}
+					</ul>
+				</div>
+				<div className="instructions">
+					<h2>Instructions</h2>
+					<ol>
+						{selectedRecipe.instructions.map((instruction, index) => (
+							<li key={index}>{instruction}</li>
+						))}
+					</ol>
+				</div>
+				{/* Back button */}
+				<Link to="/" className="goBackButton">
+					Go Back
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default RecipeList;
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 const corsOptions = {
-	origin: "http://localhost:3000", // Replace this with your client's URL for better security in production
+	origin: "*", // Replace this with your client's URL for better security in production
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 	allowedHeaders: "Content-Type,Authorization", // Adjust based on your needs
 	preflightContinue: false,
