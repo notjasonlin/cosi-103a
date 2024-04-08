8aebd2e5789445f30c83866b0ec95a0b66c54549
Jason Lin <113144409+notjasonlin@users.noreply.github.com>
2024-04-08 00:26:03 +0000

    13

--- avocado/src/App.js
+++ avocado/src/App.js
@@ -20,8 +20,11 @@ import DataInputForm from "./components/sendData";
 
 import { useEffect, useState } from "react";
 import CookMode from "./components/cook-mode";
+import os from "os"; 
+const networkInterfaces = os.networkInterfaces();
 
 function App() {
+	console.log(networkInterfaces);
     const [backendData, setBackendData] = useState([{}]);
     const ip = "100.100.197.152";
 
